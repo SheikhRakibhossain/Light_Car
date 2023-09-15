d64102169502725508c13c06814266c1ea0273ec
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-15 23:40:48 +0600

    Rang btn added

--- src/Pages/Home/MainHome/Home.jsx
+++ src/Pages/Home/MainHome/Home.jsx
@@ -1,10 +1,24 @@
-
 const Home = () => {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <input
+        type="range"
+        min={0}
+        max="100"
+        value="25"
+        className="range"
+        step="25"
+      />
+      <div className="w-full flex justify-between text-xs px-2">
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
