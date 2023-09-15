4fe54ee44991667814864687336676e91d43e10b
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-15 23:44:36 +0600

    subtotall features added

--- src/Pages/Home/MainHome/Home.jsx
+++ src/Pages/Home/MainHome/Home.jsx
@@ -27,6 +27,24 @@ const Home = () => {
           <p>tabIndex={0} attribute is necessary to make the div focusable</p>
         </div>
       </div>
+      <div className="stats bg-primary text-primary-content">
+        <div className="stat">
+          <div className="stat-title">Account balance</div>
+          <div className="stat-value">$89,400</div>
+          <div className="stat-actions">
+            <button className="btn btn-sm btn-success">Add funds</button>
+          </div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">Current balance</div>
+          <div className="stat-value">$89,400</div>
+          <div className="stat-actions">
+            <button className="btn btn-sm">Withdrawal</button>
+            <button className="btn btn-sm">deposit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
