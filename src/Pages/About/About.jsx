b703f881ff856737930bcd9b9a72ba946d600410
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-20 20:26:02 +0600

    About us page hero section created

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,26 +1,24 @@
 const About = () => {
   return (
-    <div>
-      <div className="indicator">
-        <div className="indicator-item indicator-bottom">
-          <button className="btn btn-primary">Apply</button>
-        </div>
-        <div className="card border">
-          <div className="card-body">
-            <h2 className="card-title">Job Title</h2>
-            <p>Rerum reiciendis beatae tenetur excepturi</p>
-          </div>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Let{"'"} Know About Us</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
-      <h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
-        accusamus praesentium quibusdam?
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum vitae qui
-        corrupti labore? Debitis inventore reprehenderit facere doloribus nobis
-        doloremque?
-      </p>
     </div>
   );
 };
