5ebfeb4d226152486e3d0ae81001ff07cffda3a4
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-21 18:32:57 +0600

    test api created for get api calling in backedn

--- src/Pages/TestPage/Test.jsx
+++ src/Pages/TestPage/Test.jsx
@@ -1,9 +1,30 @@
 
 const Test = () => {
+
+
+    const jwtGenerate = () =>{
+        // console.log('hello generate jwt');
+        fetch('http://localhost:5000/gen-jwt',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify({name:'Rakib&Soanli'})
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+        .catch(error =>console.log(error))
+
+    }
+
+
     return (
-        <div>
-            <h2 className="text-center font-bold py-6 text-orange-500">Hello Test</h2>
-        </div>
+        <>
+            <h2 className="text-center font-bold py-6 text-orange-500 text-5xl">Hello Test</h2>
+            <button onClick={jwtGenerate} type="button" className="btn btn-success btn-sm">Generate Token For JWT</button>
+        </>
     );
 };
 
