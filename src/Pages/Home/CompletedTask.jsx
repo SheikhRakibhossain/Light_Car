5dc6ef933db7500537e66996bcdc3ee051573447
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-24 19:00:20 +0600

    prjoect data data feactch has successfu;;

--- src/Pages/Home/CompletedTask.jsx
+++ src/Pages/Home/CompletedTask.jsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
 const CompletedTask = () => {
+
+    const [tabIndex, setTabIndex] = useState(0);
+    const [projects, setProjects] = useState(['']);
+    useEffect(()=>{
+
+        fetch('http://localhost:5000/completed-projects')
+        .then(res=>res.json())
+        .then(data =>{
+            console.log(data)
+            setProjects(data)
+        })
+        .catch(error =>console.log(error))
+    },[])
+
   return (
     <>
-      <div>
+      <div className="text-center">
+        <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)} className="border-0">
+          <TabList>
+            <Tab>title 1</Tab>
+            <Tab>Title 2</Tab>
+            <Tab>Title 2</Tab>
+            <Tab>Title 2</Tab>
+            <Tab>Title 2</Tab>
+            <Tab>Title 2</Tab>
+          </TabList>
 
-
-        
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
       </div>
     </>
   );
--- src/Pages/Home/MainHome/Home.jsx
+++ src/Pages/Home/MainHome/Home.jsx
@@ -2,6 +2,7 @@
 // import ServiceCard from "../../Service/ServiceCard";
 import Service from "../../Service/Service";
 import AboutUs from "../AboutUs";
+import CompletedTask from "../CompletedTask";
 import HomeHero from "../HomeHero";
 import WhyChooesUs from "../WhyChooesUs";
 
@@ -10,6 +11,7 @@ const Home = () => {
     <>
     <HomeHero/>
     <AboutUs/>
+    <CompletedTask/>
     <WhyChooesUs/>
     <Service/>
     </>
