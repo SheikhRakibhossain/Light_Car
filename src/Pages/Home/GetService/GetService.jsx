bda7f898731278375be4b851e0531c672830fabc
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-25 00:10:25 +0600

    Get service now section created

---
+++ src/Pages/Home/GetService/GetService.jsx
@@ -0,0 +1,17 @@
+import "./GetService.css";
+
+const GetService = () => {
+  return (
+    <>
+      <section className="grid md:grid-cols-2">
+        {/* form part */}
+        <div></div>
+
+        {/* portfolio part */}
+        <div></div>
+      </section>
+    </>
+  );
+};
+
+export default GetService;
