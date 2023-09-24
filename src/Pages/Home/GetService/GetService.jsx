b90c5ebb83fa3d24c595d80e860444d3509c559b
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-25 00:14:04 +0600

    get service now header added and home page inside added for view

--- src/Pages/Home/GetService/GetService.jsx
+++ src/Pages/Home/GetService/GetService.jsx
@@ -5,7 +5,14 @@ const GetService = () => {
     <>
       <section className="grid md:grid-cols-2">
         {/* form part */}
-        <div></div>
+        <div>
+          <div className="py-10 ">
+            <h2 className="text-3xl font-semibold text-center">
+            Get A{" "}
+              <span className="text-[#f22613]">Service Now !</span>
+            </h2>
+          </div>
+        </div>
 
         {/* portfolio part */}
         <div></div>
--- src/Pages/Home/MainHome/Home.jsx
+++ src/Pages/Home/MainHome/Home.jsx
@@ -2,6 +2,7 @@
 import Service from "../../Service/Service";
 import AboutUs from "../AboutUs";
 import CompletedTask from "../CompletedTask";
+import GetService from "../GetService/GetService";
 import HomeHero from "../HomeHero";
 import Pricing from "../Pricing/Pricing";
 import WhyChooesUs from "../WhyChooesUs";
@@ -9,6 +10,7 @@ import WhyChooesUs from "../WhyChooesUs";
 const Home = () => {
   return (
     <>
+    <GetService/>
     <HomeHero/>
     <AboutUs/>
     <WhyChooesUs/>
