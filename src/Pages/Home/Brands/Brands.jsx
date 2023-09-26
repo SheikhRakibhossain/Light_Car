29c1a837bf6eb1cb519ff45b751edd62c3d379fe
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-26 10:15:13 +0600

    maquee brand has added

--- src/Pages/Home/Brands/Brands.jsx
+++ src/Pages/Home/Brands/Brands.jsx
@@ -1,10 +1,21 @@
 import Marquee from "react-fast-marquee";
+import images from './Logo' 
+
 const Brands = () => {
+
+    // console.log(images)
   return (
-    <div>
-      <Marquee>
-        I can be a React component, multiple React components, or just some
-        text.
+    <div className="bg-[#ebebebd4] px-10 py-10">
+      <div className="pt-10 pb-5">
+        <h2 className="text-3xl font-semibold text-center">
+          Our Services <span className="text-[#f22613]">Brand !</span>
+        </h2>
+      </div>
+      <Marquee speed={150}>
+       
+      {
+        images.map(image=><img key={image} src={image} alt="car logo" className="w-[150px] p-4 " />  )
+      }
       </Marquee>
     </div>
   );
---
+++ src/Pages/Home/Brands/Logo.jsx
@@ -0,0 +1,21 @@
+import img1 from "../../../assets/images/Brands/1.png";
+import img2 from "../../../assets/images/Brands/2.png";
+import img3 from "../../../assets/images/Brands/3.png";
+import img4 from "../../../assets/images/Brands/4.png";
+import img5 from "../../../assets/images/Brands/5.png";
+import img6 from "../../../assets/images/Brands/6.png";
+import img7 from "../../../assets/images/Brands/7.png";
+import img8 from "../../../assets/images/Brands/8.png";
+import img9 from "../../../assets/images/Brands/9.png";
+import img10 from "../../../assets/images/Brands/10.png";
+import img11 from "../../../assets/images/Brands/11.png";
+import img12 from "../../../assets/images/Brands/12.png";
+import img13 from "../../../assets/images/Brands/13.png";
+import img14 from "../../../assets/images/Brands/14.png";
+import img15 from "../../../assets/images/Brands/15.png";
+import img16 from "../../../assets/images/Brands/16.png";
+import img17 from "../../../assets/images/Brands/17.png";
+import img18 from "../../../assets/images/Brands/18.png";
+import img19 from "../../../assets/images/Brands/19.png";
+
+export default[img1, img2, img3, img4, img5, img6, img7, img8, img9,img10, img11, img12, img13, img14, img15,img16, img17, img18, img19];
