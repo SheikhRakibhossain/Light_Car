538932fd13b89bbcaf6d19453c5816b1725c0cec
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-27 12:54:19 +0600

    team card design

--- src/Pages/About/Team.jsx
+++ src/Pages/About/Team.jsx
@@ -1,98 +1,49 @@
-// import { FaFacebook, FaTwitter } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 import "./style.css";
 
 const Team = () => {
-//   const socialMediaIcons = [
-//     {
-//       name: "Twitter",
-//       icon: (
-//        <FaFacebook/>
-//       ),
-//       link: "https://twitter.com/",
-//     },
-//     {
-//       name: "Instagram",
-//       icon: (
-//         <FaTwitter/>
-//       ),
-//       link: "https://www.instagram.com/example",
-//     },
-//     {
-//       name: "Instagram",
-//       icon: (
-//         <FaTwitter/>
-//       ),
-//       link: "https://www.instagram.com/example",
-//     },
-//     {
-//       name: "Instagram",
-//       icon: (
-//         <FaTwitter/>
-//       ),
-//       link: "https://www.instagram.com/example",
-//     },
-//     {
-//       name: "Instagram",
-//       icon: (
-//         <FaTwitter/>
-//       ),
-//       link: "https://www.instagram.com/example",
-//     }
-//     // Add more social media icons as needed
-//   ];
-
-const socialMediaIcons =[
-
-    {
-        icon:"hi",
-        link: "a"
-    },
-    {
-        icon:"hi",
-        link: "a"
-    },
-    {
-        icon:"hi",
-        link: "a"
-    },
-    {
-        icon:"hi",
-        link: "a"
-    },
-    {
-        icon:"hi",
-        link: "a"
-    },
-    {
-        icon:"hi",
-        link: "a"
-    },
-]
-
-
-return (
+  return (
     <>
       <div className="max-w-sm mx-auto p-4">
-        <div className="relative group">
+        <div className="relative group overflow-hidden">
           {/* Card container */}
-          <div className="bg-white border rounded-lg shadow-lg p-4 transition-transform transform hover:scale-105">
-            <h2 className="text-xl font-semibold mb-2">User Name</h2>
-            <p className="text-gray-600">Description of the user.</p>
+          <div className="bg-white border rounded-lg shadow-lg cursor-pointer">
+            <img
+              src="https://img.freepik.com/free-photo/young-mechanic-working-his-workshop_23-2148970733.jpg?size=626&ext=jpg&ga=GA1.1.28436747.1695030037&semt=ais"
+              alt="car maker"
+            />
           </div>
-          {/* Social media icons container (hidden by default) */}
-          <div className="absolute bottom-0  hidden group-hover:block p-2 space-x-2 bg-white transform -translate-y-0 transition-transform duration-5000 ease-out hover:ease-in">
-            {socialMediaIcons.map((icon, index) => (
-              <a
-                key={index}
-                href={icon.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:text-blue-700 "
-              >
-                {icon.icon}
-              </a>
-            ))}
+          {/* Social media icons container */}
+          <div className="absolute bottom-0 left-0 w-full bg-[#0000006f]  py-2 px-4 flex justify-center space-x-2 transform translate-y-full transition-transform duration-300 group-hover:translate-y-0 text-[30px] ">
+            <a
+              href="#"
+              className="hover:text-[#1877f2] text-blue-400 border p-2"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href="#"
+              className="hover:text-[#1da1f2] text-blue-300 border p-2"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="#"
+              className="hover:text-[#c32aa3] text-pink-200 border p-2"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="#"
+              className="hover:text-[#0a66c2] text-blue-300 border p-2"
+            >
+              <FaLinkedin />
+            </a>
           </div>
+        </div>
+        <div className="text-center border">
+          <h2 className="text-xl font-semibold mb-2">Robert Hearter</h2>
+          <span className=" bg-slate-600 text-white py-1 px-5 rounded-full">Color Designer</span>
         </div>
       </div>
     </>
